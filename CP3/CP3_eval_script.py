b16024912c61b98a2c39029d66b7a6904a95be4a
Mikhail Korobov <kmike84@gmail.com>
2016-08-04 10:48:05 -0400

    normalize URLs; add a progress bar

--- CP3/CP3_eval_script.py
+++ CP3/CP3_eval_script.py
@@ -1,31 +1,57 @@
 #!/usr/bin/env python
 from __future__ import print_function
+import re
 import sys
+
 import tldextract
-import json
+from six.moves.urllib.parse import urlparse
+try:
+    from tqdm import tqdm
+except ImportError:
+    tqdm = lambda it, *args: it
+# it requires w3lib >= 1.15.0
+from w3lib.url import canonicalize_url
+
 
 # how to use: python CP3_eval_script.py ground_truth_sample_CP3.json submission_sample_CP3.json output_sample_CP3.txt
 output_file = open(sys.argv[3], "w")
+
+
+# XXX: copied from Scrapy
+def add_http_if_no_scheme(url):
+    """Add http as the default scheme if it is missing from the url."""
+    match = re.match(r"^\w+://", url, flags=re.I)
+    if not match:
+        parts = urlparse(url)
+        scheme = "http:" if parts.netloc else "http://"
+        url = scheme + url
+
+    return url
 
 
 def get_domain(url):
     return tldextract.extract(url).registered_domain.lower()
 
 
+def normalize_url(url):
+    url = add_http_if_no_scheme(url)
+    return canonicalize_url(url).replace('https://', 'http://', 1)
+
+
 ################################################
 # ground truth data
 gt_outputs = open(sys.argv[1], "r")
 gt_sites = [line.rstrip('\n') for line in gt_outputs]
-gt_domains = set([get_domain(url) for url in gt_sites])
-gt_urls = set([url.split("://")[-1] for url in gt_sites])
+gt_domains = {get_domain(url) for url in gt_sites}
+gt_urls = {normalize_url(url) for url in gt_sites}
 ################################################
 
 ################################################
 # submission data
 sub_outputs = open(sys.argv[2], "r")
-sub_sites = [line.rstrip('\n') for line in sub_outputs]
-sub_domains = set([get_domain(url) for url in sub_sites])
-sub_urls = set([url.split("://")[-1] for url in sub_sites])
+sub_sites = [line.rstrip('\n') for line in tqdm(sub_outputs, "reading submission")]
+sub_domains = {get_domain(url) for url in tqdm(sub_sites, "extracting domains")}
+sub_urls = {normalize_url(url) for url in tqdm(sub_sites, "normalizing urls")}
 ################################################
 
 
